f11b5f93c272f3bdedd5af92e1eff356f05992ba
rosasthomas <rosasthomas@gmail.com>
2019-04-11 13:03:37 -0300

    TP1 terminado 11/04/19

---
+++ tp1 labo 2/Rosas.Thomas.TP2/Entidades/Calculadora.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        private static string ValidarOperador(string operador)
+        {
+            string auxiliar = "+";
+
+            if (operador == "+" || operador == "-" || operador == "/" || operador == "*")
+            {
+                auxiliar = operador;
+            }
+
+            return auxiliar;
+        }
+        public static double Operar(Numero numUno, Numero numDos, string operador)
+        {
+            double retorno;
+            
+            switch (ValidarOperador(operador))
+            {
+                case "+":
+                    retorno = numUno + numDos;
+                        break;
+                case "-":
+                    retorno = numUno - numDos;
+                    break;
+                case "*":
+                    retorno = numUno * numDos;
+                    break;
+                case "/":
+                    retorno = 0;
+                    if (!Equals(numDos, 0))
+                        retorno = numUno / numDos;
+                    if (double.IsInfinity(retorno))
+                        retorno = double.MinValue;
+                    break;
+                default:
+                    retorno = 0;
+                    break;
+            }
+            return retorno;
+        }
+    }
+}
---
+++ tp1 labo 2/Rosas.Thomas.TP2/Entidades/Numero.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        private double numero;
+
+        public Numero() :this(0)
+        {
+
+        }
+
+        public Numero(double auxNumero)
+        {
+            numero = auxNumero;
+        }
+        public Numero(string strNumero)
+        {
+            this.SetNumero = strNumero;
+        }
+
+        private double ValidarNumero(string strNumero)
+        {            
+            if (!double.TryParse(strNumero, out double auxiliar))
+            {
+                auxiliar = 0;
+            }
+
+            return auxiliar;
+        }
+
+        private string SetNumero
+        {
+            set
+            {
+               this.numero = ValidarNumero(value);
+            }
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            string decimalNum = "Valor inválido";
+
+            if (binario == double.MinValue.ToString() || int.Parse(binario) < 0 || int.Parse(binario) > 1)
+            {
+              decimalNum = binario;
+            }
+            else if(binario != decimalNum && binario != "" && int.Parse(binario) >= 0 && int.Parse(binario) <= 1)
+            {
+                decimalNum = Convert.ToInt32(binario,2).ToString();
+            }
+             
+                      
+            return decimalNum;
+        }
+        public string DecimalBinario(string numero)
+        {
+            string retorno = "Valor inválido";
+            if (numero != "Valor inválido" && numero != "" && numero != double.MinValue.ToString())
+            {
+                int auxNumero = Convert.ToInt32(numero);
+                if (auxNumero > 0)
+                {
+                    while (auxNumero > 0)
+                    {
+                        if (auxNumero % 2 == 0)
+                        {
+                            retorno = "0" + retorno;
+                        }
+                        else
+                        {
+                            retorno = "1" + retorno;
+                        }
+                        auxNumero = (int)(auxNumero / 2);
+                    }
+                }
+                else
+                {
+                    if (auxNumero == 0)
+                    {
+                        retorno = "0";
+                    }
+                    else
+                    {
+                        retorno = "Valor inválido";
+                    }
+                }
+            }
+            return retorno;
+        }
+        public string DecimalBinario(double numero)
+        {
+            return DecimalBinario(numero.ToString());
+        }
+
+        public static double operator +(Numero numeroUno, Numero numeroDos)
+        {
+            return numeroUno.numero + numeroDos.numero;
+        }
+        public static double operator -(Numero numeroUno, Numero numeroDos)
+        {
+            return numeroUno.numero - numeroDos.numero;
+        }
+        public static double operator *(Numero numeroUno, Numero numeroDos)
+        {
+            return numeroUno.numero * numeroDos.numero;
+        }
+        public static double operator /(Numero numeroUno, Numero numeroDos)
+        {
+            return numeroUno.numero / numeroDos.numero;
+        }
+    }
+}
